15afc11949e246fb213be4c3f1bbff2ff5488693
Caleb Sherfick <calebsherfick@gmail.com>
2019-04-20 14:25:35 -0600

    fixed vault delete button

--- wwwsrc/src/components/Vault.vue
+++ wwwsrc/src/components/Vault.vue
@@ -1,13 +1,15 @@
 <template>
 
-  <div @click="vaultKeeps(vault.id); setActiveVault(vault)"
+  <div
     :class="$mq | mq({xs: 'col-12 mb-3 clickable', sm: 'col-12 mb-3 clickable', md: 'col-4 mb-3 clickable', lg: 'col-3 mb-3 clickable'})">
     <div class="card">
-      <div class="card-header">
-        {{vault.name}}
-      </div>
-      <div class="card-body">
-        <p class="card-text">{{vault.description}}</p>
+      <div @click="vaultKeeps(vault.id); setActiveVault(vault)">
+        <div class="card-header">
+          {{vault.name}}
+        </div>
+        <div class="card-body">
+          <p class="card-text">{{vault.description}}</p>
+        </div>
       </div>
       <button class="btn btn-danger px-4 delete-btn" @click="deleteVault(vault.id)">Delete</button>
     </div>
