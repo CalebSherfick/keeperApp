6f76a4495a22b787d0817fb026404666554cdd2c
Caleb Sherfick <calebsherfick@gmail.com>
2019-04-20 21:18:11 -0600

    mobile nav-bar

--- wwwsrc/src/components/Navbar.vue
+++ wwwsrc/src/components/Navbar.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="navComponent">
 
+    <mq-layout mq="xs">
+      <nav class="navbar m-0 keeprBG row justify-content-start">
+        <div class="col-1 d-flex justify-content-center">
+          <radial-menu style="background-color: #f2f2f2" :itemSize="50" :size=50 :radius="90" :rotate=225
+            :angle-restriction="45">
+            <radial-menu-item v-for="(item, index) in items" :key="index" style="background-color: #f2f2f2"
+              @click="() => handleClick(item)">
+              <span>{{item}}</span>
+            </radial-menu-item>
+          </radial-menu>
+        </div>
+        <div class="offset-10 col-1 d-flex justify-content-center">
+          <i class="fas fa-bars fa-3x" data-toggle="collapse" data-target="#nav-bar-collapse"></i>
+        </div>
 
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="navbar-collapse" id="nav-bar-collapse">
+          <ul class="nav navbar-nav">
+            <a class="nav-text-mobile col-12" @click="clearKeeps(); goVaults()">My
+              Vaults</a>
+            <a class="nav-text-mobile col-12" @click="clearKeeps(); goMyKeeps()">My Keeps</a>
+            <a class="nav-text-mobile col-12" @click="logout">Logout</a>
+          </ul>
+        </div>
+
+
+
+
+
+      </nav>
+    </mq-layout>
 
     <mq-layout mq="md">
       <nav
         :class="$mq | mq({xs: 'navbar keeprBG row', sm: 'navbar keeprBG row', md: 'navbar navbar-expand-lg keeprBG row', lg: 'navbar navbar-expand-lg keeprBG row'})">
-
         <a class="nav-text" @click="clearKeeps(); goDashboard()"
           :class="$mq | mq({xs: 'col-12', sm: 'col-12', md: 'col-1 offset-2', lg: 'col-1 offset-2'})">Keepr</a>
         <div
@@ -14,14 +43,10 @@
           <button type="button" class="btn create-btn" data-toggle="modal" data-target="#createVaultMod">
             <i class="fas fa-plus"></i> New Vault
           </button>
-
           <button type="button" class="btn create-btn ml-1" data-toggle="modal" data-target="#createKeepMod">
             <i class="fas fa-plus"></i> New Keep
           </button>
-
         </div>
-
-
         <a class="nav-text" @click="clearKeeps(); goVaults()"
           :class="$mq | mq({xs: 'col-12', sm: 'col-12', md: 'col-1', lg: 'offset-3 col-1'})">My
           Vaults</a>
@@ -35,7 +60,6 @@
     <mq-layout mq="lg">
       <nav
         :class="$mq | mq({xs: 'navbar keeprBG row', sm: 'navbar keeprBG row', md: 'navbar navbar-expand-lg keeprBG row', lg: 'navbar navbar-expand-lg keeprBG row'})">
-
         <a class="nav-text" @click="clearKeeps(); goDashboard()"
           :class="$mq | mq({xs: 'col-12', sm: 'col-12', md: 'col-1 offset-2', lg: 'col-1 offset-2'})">Keepr</a>
         <div
@@ -47,10 +71,7 @@
           <button type="button" class="btn create-btn ml-1" data-toggle="modal" data-target="#createKeepMod">
             <i class="fas fa-plus"></i> New Keep
           </button>
-
         </div>
-
-
         <a class="nav-text" @click="clearKeeps(); goVaults()"
           :class="$mq | mq({xs: 'col-12', sm: 'col-12', md: 'col-1', lg: 'offset-3 col-1'})">My
           Vaults</a>
@@ -60,17 +81,18 @@
           :class="$mq | mq({xs: 'col-12', sm: 'col-12', md: 'col-1', lg: 'col-1'})">Logout</a>
       </nav>
     </mq-layout>
-
-
   </div>
 </template>
 
 <script>
+  import { RadialMenu, RadialMenuItem } from 'vue-radial-menu'
   export default {
     name: "nav-component",
     props: [],
     data() {
-      return {};
+      return {
+        items: ['Keep', 'Vault']
+      };
     },
     computed: {},
     methods: {
@@ -89,9 +111,19 @@
       },
       goMyKeeps() {
         this.$router.push({ path: '/mykeeps' })
+      },
+      handleClick(item) {
+        if (item == "Keep") {
+          $('#createKeepMod').modal('show')
+        } else if (item == "Vault") {
+          $('#createVaultMod').modal('show')
+        }
       }
     },
-    components: {}
+    components: {
+      RadialMenu,
+      RadialMenuItem
+    }
   };
 </script>
 
